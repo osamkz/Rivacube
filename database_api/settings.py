0e35df3de91ad2f2011e8bd9bdf8d86cf14046df
Kevin Besson <k.besson@icmaae.foundation>
2022-08-10 13:30:47 +0000

    Set pagination to 100,000 instead of 100

--- database_api/settings.py
+++ database_api/settings.py
@@ -136,7 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100000
 }
 
 USE_THOUSAND_SEPARATOR = True
