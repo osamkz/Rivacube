23467b3a5c20c55f9b2a4905b4174d3b0e2fbfb0
Kevin Besson <k.besson@icmaae.foundation>
2022-08-12 13:21:43 +0000

    Added localhost to ALLOWED_HOST

--- database_api/settings.py
+++ database_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['195.15.241.6', 'localhost']
 
 
 # Application definition
