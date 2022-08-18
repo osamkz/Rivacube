a46389b5618dff35e4c9ad91057d39eb7762494d
Kevin Besson <k.besson@icmaae.foundation>
2022-08-18 09:15:56 +0000

    + htag column & mentions_screen_name -> ArrayField

--- media/models.py
+++ media/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
+from django.contrib.postgres.fields import ArrayField
+
 # Create your models here.
 
 # TODO: remettre une foreign key sur reply_to_status_id
 # reply_to_status_id = models.ForeignKey('self', on_delete = models.CASCADE, default = None, blank = True, null = True)
-# TODO: Creer un array pour mentions_screen_name
 
 
 class Media(models.Model):
     status_id = models.BigIntegerField("status_id", primary_key=True)
+    htag = models.CharField("htag", max_length=32, default="To be defined")
     user_id = models.BigIntegerField("user_id")
     created_at = models.DateTimeField("created_at")
     screen_name = models.CharField("screen_name", max_length=32)
@@ -20,12 +22,15 @@ class Media(models.Model):
     reply_to_user_id = models.BigIntegerField(
         "reply_to_user_id", default=None, blank=True, null=True)
     reply_to_screen_name = models.CharField(
-        "reply_to_screen_name", max_length=32, default=None, blank=True, null=True)
+        "reply_to_screen_name", max_length=32, null=True)
     is_quote = models.BooleanField("is_quote")
     is_retweet = models.BooleanField("is_retweet")
     favorite_count = models.SmallIntegerField("favorite_count")
     retweet_count = models.IntegerField("retweet_count")
-    mentions_screen_name = models.CharField(
-        "mentions_screen_name", max_length=1024)
+    mention_screen_name = ArrayField(
+        models.CharField(
+            max_length=32
+        ),
+        null=True)
     lang = models.CharField("lang", max_length=3)
-    description = models.TextField("description")
+    description = models.TextField("description", null=True)
