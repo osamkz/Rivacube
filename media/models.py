1ee951925f5c451bfd47e711c55c3c0705ae652f
Kevin Besson <k.besson@icmaae.foundation>
2022-08-20 22:44:25 +0000

    Updated max value for some fields

--- media/models.py
+++ media/models.py
@@ -13,7 +13,7 @@ class Media(models.Model):
     htag = models.CharField("htag", max_length=32, default="To be defined")
     user_id = models.BigIntegerField("user_id")
     created_at = models.DateTimeField("created_at")
-    screen_name = models.CharField("screen_name", max_length=32)
+    screen_name = models.CharField("screen_name", max_length=16)
     text = models.TextField("text")
     source = models.CharField("source", max_length=64)
     display_text_width = models.SmallIntegerField("display_text_width")
@@ -22,14 +22,14 @@ class Media(models.Model):
     reply_to_user_id = models.BigIntegerField(
         "reply_to_user_id", default=None, blank=True, null=True)
     reply_to_screen_name = models.CharField(
-        "reply_to_screen_name", max_length=32, null=True)
+        "reply_to_screen_name", max_length=16, null=True)
     is_quote = models.BooleanField("is_quote")
     is_retweet = models.BooleanField("is_retweet")
-    favorite_count = models.SmallIntegerField("favorite_count")
+    favorite_count = models.IntegerField("favorite_count")
     retweet_count = models.IntegerField("retweet_count")
     mentions_screen_name = ArrayField(
         models.CharField(
-            max_length=32
+            max_length=64
         ),
         null=True)
     lang = models.CharField("lang", max_length=3)
